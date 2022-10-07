c9c0ef2af97fc4b32cd061c91d20fee064d6c5a8
Eric Kim <bosung90@gmail.com>
2022-10-07 16:31:29 -0700

    change react to preact

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,12 @@
-// import CountButton from "./CountButton";
-// import { useState } from "react";
+import { useState } from "preact/hooks";
 
 let count = 0;
 export default function Home() {
-  // const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Home</h1>
-      {/* <CountButton /> */}
-      <button onClick={() => alert("hello world")}>{count}</button>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
     </div>
   );
 }
-
-// tsx(jsx) -> js -> client
-// tsx(jsx) -> js -> html -> client
